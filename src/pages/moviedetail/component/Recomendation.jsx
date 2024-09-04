848d2fa28801434ed51e57eb052e5baba4e37f48
Byunh0 <bhy1999313@naver.com>
2024-09-04 12:28:10 +0900

    retry

--- src/pages/moviedetail/component/Recomendation.jsx
+++ src/pages/moviedetail/component/Recomendation.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import { useRecomendation } from '../../../hook/useRecomendation'
 import MovieSlider from '../../../common/MovieSlider/MovieSlider'
 import { responsive } from '../../../constants/responsive'
+import { ClipLoader } from 'react-spinners'
+import { Alert } from 'react-bootstrap'
 
 const Recomendation = ({id}) => {
     
-    const {data}=useRecomendation({id})
-    console.log("recmo",data)
-   
+    const {data,isLoading,isError,error}=useRecomendation({id})
+
+    if(isLoading){
+      return  <div className="bigContainer"><ClipLoader color ="#f88c6b" loading={isLoading} size={70} className="cliploader"/></div>
+  }
+  if(isError){
+      return <Alert varient ="danger">{error.message}</Alert>
+  }
+  console.log("recmo",data)
   return (
     <div>
-  {/* <MovieSlider title={"Recomendation"} movies={data?.results} responsive={responsive}/> */}
+  <MovieSlider title={"Recomendation"} movies={data?.results} responsive={responsive}/>
     </div>
   )
 }
