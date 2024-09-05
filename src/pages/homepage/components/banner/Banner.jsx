d96bbda59a1eee75363e90f841d1c139908c895d
Byunh0 <bhy1999313@naver.com>
2024-09-05 23:53:07 +0900

    bannerwrong6

--- src/pages/homepage/components/banner/Banner.jsx
+++ src/pages/homepage/components/banner/Banner.jsx
@@ -4,7 +4,7 @@ import ClipLoader from "react-spinners/ClipLoader"
 
 import Alert from 'react-bootstrap/Alert';
 import './Banner.style.css'
-
+import Button from 'react-bootstrap/Button';
 
 const Banner = ({setSpoiler}) => {
   
@@ -24,7 +24,7 @@ const Banner = ({setSpoiler}) => {
    <div className="text-white banner-text-area1">
 <h1>{data?.results[0].title}</h1>
 <p>{data && data.results[0].overview}</p>
-<button onClick={gospoiler}>예고편</button>
+<Button variant="light"  onClick={gospoiler}>예고편</Button>
    </div>
     </div>
     
