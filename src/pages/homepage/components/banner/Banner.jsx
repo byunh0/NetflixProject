c078e219e347da7dbf151a89912156a97a4980b2
Byunh0 <bhy1999313@naver.com>
2024-09-05 23:07:00 +0900

    bannerwrong

--- src/pages/homepage/components/banner/Banner.jsx
+++ src/pages/homepage/components/banner/Banner.jsx
@@ -4,7 +4,6 @@ import ClipLoader from "react-spinners/ClipLoader"
 
 import Alert from 'react-bootstrap/Alert';
 import './Banner.style.css'
-import Button from 'react-bootstrap/Button';
 
 
 const Banner = ({setSpoiler}) => {
@@ -16,18 +15,21 @@ const Banner = ({setSpoiler}) => {
     console.log("ddd",data)
     if (isLoading) {return <div className="bigContainer"><ClipLoader color ="#f88c6b" loading={isLoading} size={70} /></div> } ;
     if (isError) return<Alert  variant="danger">{error.message}</Alert>;
-    
+  
   return (
     <div >
+  
     <div style={{backgroundImage:"url("+`https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data.results[0].poster_path}`+")"}}
     className="banner">
      
    <div className="text-white banner-text-area">
 <h1>{data?.results[0].title}</h1>
 <p>{data && data.results[0].overview}</p>
-<Button variant="light"  onClick={gospoiler}>Play</Button>
+<button onClick={gospoiler}>예고편</button>
    </div>
-
+   <div>
+  
+</div>
     </div>
     </div>
   )
