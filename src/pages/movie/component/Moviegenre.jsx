b86ad6a7f373f7d19d4dc61658cf6e45091a6799
Byunh0 <bhy1999313@naver.com>
2024-09-06 10:54:45 +0900

    selectfilter

--- src/pages/movie/component/Moviegenre.jsx
+++ src/pages/movie/component/Moviegenre.jsx
@@ -1,27 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useMovieGenreQuery } from '../../../hook/useMovieGenre';
 import { useSearchMovieQuery } from '../../../hook/useSearchMovieQuery';
 import Badge from 'react-bootstrap/Badge';
-const Moviegenre = ({page}) => {
-    const[newgenre,setNewgenre]=useState("")
+import { useNavigate } from 'react-router-dom';
+
+const Moviegenre = () => {
+    const [newgenre, setNewgenre] = useState("");
+    const navigate=useNavigate();
+    const { data: genreData } = useMovieGenreQuery();
+    console.log("genre", genreData);
+    // const allpage=()=>{
+    //   navigate('/movie')
+    // }
+    useEffect(() => {
+      if (newgenre && genreData && genreData.length > 0) {
+        console.log("newgenre", newgenre);
+        navigate(`/movie?q=${newgenre}`);
+    }
+}, [newgenre, genreData]);
+
    
-    useEffect(()=>{
-        console.log("newgenre",newgenre);
-    },[newgenre])
-    const {data:genreData}=useMovieGenreQuery();
-    console.log("genre",genreData)
-  return (
-  <div>
-   <Form.Select aria-label="Default select example">
-      <option>select All</option>
-     {genreData.map((id)=><option value="1">{id.name}</option>)} 
-     
-    </Form.Select>
-  </div>
-  )
-}
 
-export default Moviegenre
+    const handleGenreChange = (event) => {
+       event.target.value.length==0?  navigate('/movie'):setNewgenre(event.target.value);
+    };
 
+    return (
+        <div>
+            <Form.Select aria-label="Default select example" onChange={handleGenreChange}>
+                <option value="">All page</option>
+                {genreData && genreData.map((genre) => (
+                    <option key={genre.id} value={genre.name}>
+                        {genre.name}
+                    </option>
+                ))}
+            </Form.Select>
+            {/* <button onClick={allpage}>All page</button> */}
+        </div>
+    );
+};
+
+export default Moviegenre;
